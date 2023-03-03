07105e2ab298771baa55f362dade275e21a6a85d
Yin Jie <mumingv@163.com>
2023-03-03 17:58:01 +0800

    后端管理平台：增加路由和控制器

---
+++ controller/admin.go
@@ -0,0 +1,71 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type AdminController struct {
+}
+
+// Login 登录
+func (a *AdminController) Login(c *gin.Context) {
+
+}
+
+// Logout 退出
+func (a *AdminController) Logout(c *gin.Context) {
+
+}
+
+// Main 主页
+func (a *AdminController) Main(c *gin.Context) {
+
+}
+
+// Config 系统配置信息展示
+func (a *AdminController) Config(c *gin.Context) {
+
+}
+
+// AddConfig 系统配置信息更新
+func (a *AdminController) AddConfig(c *gin.Context) {
+
+}
+
+// Index 后台首页
+func (a *AdminController) Index(c *gin.Context) {
+
+}
+
+// Article 博文添加
+func (a *AdminController) Article(c *gin.Context) {
+
+}
+
+// Save 保存
+func (a *AdminController) Save(c *gin.Context) {
+
+}
+
+// PostDel 文章删除
+func (a *AdminController) PostDel(c *gin.Context) {
+
+}
+
+// Category 类目
+func (a *AdminController) Category(c *gin.Context) {
+
+}
+
+// Categoryadd 添加修改类目
+func (a *AdminController) CategoryAdd(c *gin.Context) {
+
+}
+
+// CategorySave 保存类目
+func (a *AdminController) CategorySave(c *gin.Context) {
+
+}
+
+// CategoryDel 类目删除
+func (a *AdminController) CategoryDel(c *gin.Context) {
+
+}
